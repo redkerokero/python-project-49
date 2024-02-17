1b27f018575dacebdc70d728e521d7795c3f6e13
Dmitriy <kerokero@yandex.ru>
2024-02-17 15:45:03 +0000

    rename variable

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,10 +1,10 @@
 import prompt
 
 
-def main(game_description, game_data):
+def main(game_task, game_data):
     name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
     print(f'Hello, {name}!')
-    print(game_description)
+    print(game_task)
     for game_round, round_data in enumerate(game_data):
         question, correct_answer = round_data
         print(f'Question: {question}')
