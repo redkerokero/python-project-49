ac79be4116565adf6759ad9a10fb0df90c075766
Dmitriy <kerokero@yandex.ru>
2024-02-18 18:06:26 +0000

    add brain-progression game

---
+++ brain_games/games/progression.py
@@ -0,0 +1,47 @@
+import random
+from brain_games import engine
+
+
+def generate_progression(
+    progression_length,
+    max_number=99,
+    delta_min=-10,
+    delta_max=10,
+):
+    delta = random.randint(delta_min, delta_max)
+    progression_item = str(random.randint(1, max_number))
+    progression = [progression_item]
+    for _ in range(progression_length - 1):
+        progression_item = int(progression_item) + delta
+        progression.append(str(progression_item))
+    return progression
+
+
+def hide_list_item(input_list):
+    output_list = []
+    rand_elem_index = random.randint(0, len(input_list) - 1)
+    for index, element in enumerate(input_list):
+        if index == rand_elem_index:
+            output_list.append('..')
+            hiden_element = element
+        else:
+            output_list.append(element)
+    return (output_list, hiden_element)
+
+
+def progression_game():
+    game_task = 'Find the greatest common divisor of given numbers.'
+    amount_of_rounds = 3
+    min_progression_length = 5
+    max_progression_length = 15
+    game_data = []
+    for _ in range(amount_of_rounds):
+        progression_length = random.randint(
+            min_progression_length,
+            max_progression_length,
+        )
+        progression = generate_progression(progression_length)
+        question, answer = hide_list_item(progression)
+        question = ' '.join(question)
+        game_data.append((question, answer))
+    engine.main(game_task, game_data)
