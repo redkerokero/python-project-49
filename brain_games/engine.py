33193568bba008d21904d13969354aa1a1a609b8
Dmitriy <kerokero@yandex.ru>
2024-02-23 15:43:22 +0000

    Redesign engine
    The engine module transmitted to the game and launched from it. This has been changed.
    The engine module has been redesigned so that games can be launched from it.

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,12 +1,12 @@
 import prompt
 
 
-def main(game_task, game_data):
+def main(game_task, amount_of_rounds, max_number, game):
     name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
     print(f'Hello, {name}!')
     print(game_task)
-    for game_round, round_data in enumerate(game_data):
-        question, correct_answer = round_data
+    for game_round in range(amount_of_rounds):
+        question, correct_answer = game(max_number)
         print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
         if answer == correct_answer:
@@ -17,5 +17,5 @@ def main(game_task, game_data):
                 f"'{correct_answer}'.\nLet's try again, {name}!",
             )
             break
-        if game_round == len(game_data) - 1:
+        if game_round == amount_of_rounds - 1:
             print(f'Congratulations, {name}!')
