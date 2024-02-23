7ee518e63feb68a45bdca809739b2996a9fc0c1a
Dmitriy <kerokero@yandex.ru>
2024-02-23 17:50:21 +0000

    Redesign brain-calc game
    The game module generated data for all rounds
    and a common block were transferred to the engine,
    the engine was launched from the game module. This has been changed.
    In the game module, data is generated for one round and is not accumulated;
    the engine does not start.
    The necessary constants are moved outside the function.

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,25 +1,23 @@
 import random
-from brain_games import engine
 
 
-def run_calc_game():
-    game_task = 'What is the result of the expression?'
-    amount_of_rounds = 3
-    max_number = 99
-    game_data = []
-    for _ in range(amount_of_rounds):
-        first_number = random.randint(1, max_number)
-        second_number = random.randint(1, max_number)
-        operation = random.choice(['+', '-', '*'])
-        match operation:
-            case '+':
-                question = f'{first_number} + {second_number}'
-                answer = str(first_number + second_number)
-            case '-':
-                question = f'{first_number} - {second_number}'
-                answer = str(first_number - second_number)
-            case '*':
-                question = f'{first_number} * {second_number}'
-                answer = str(first_number * second_number)
-        game_data.append((question, answer))
-    engine.main(game_task, game_data)
+GAME_TASK = 'What is the result of the expression?'
+AMOUNT_OF_ROUNDS = 3
+MAX_NUMBER = 99
+
+
+def run_calc_game(max_number):
+    first_number = random.randint(1, max_number)
+    second_number = random.randint(1, max_number)
+    operation = random.choice(['+', '-', '*'])
+    match operation:
+        case '+':
+            question = f'{first_number} + {second_number}'
+            answer = str(first_number + second_number)
+        case '-':
+            question = f'{first_number} - {second_number}'
+            answer = str(first_number - second_number)
+        case '*':
+            question = f'{first_number} * {second_number}'
+            answer = str(first_number * second_number)
+    return (question, answer)
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+from brain_games import engine
 from brain_games.games import calc
 
 
 def main():
-    calc.run_calc_game()
+    game_task = calc.GAME_TASK
+    amount_of_rounds = calc.AMOUNT_OF_ROUNDS
+    max_number = calc.MAX_NUMBER
+    game = calc.run_calc_game
+    engine.main(game_task, amount_of_rounds, max_number, game)
 
 
 if __name__ == '__main__':
