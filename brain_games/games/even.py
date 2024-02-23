1203f92603c2490578f75eaaadc117839a5628f1
Dmitriy <kerokero@yandex.ru>
2024-02-23 17:51:48 +0000

    Redesign brain-even game
    The game module generated data for all rounds
    and a common block were transferred to the engine,
    the engine was launched from the game module. This has been changed.
    In the game module, data is generated for one round and is not accumulated;
    the engine does not start.
    The necessary constants are moved outside the function.

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,17 +1,14 @@
 import random
-from brain_games import engine
+
+GAME_TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
+AMOUNT_OF_ROUNDS = 3
+MAX_NUMBER = 99
 
 
-def run_even_game():
-    game_task = 'Answer "yes" if the number is even, otherwise answer "no".'
-    amount_of_rounds = 3
-    max_number = 99
-    game_data = []
-    for _ in range(amount_of_rounds):
-        question = random.randint(1, max_number)
-        if question % 2 == 0:
-            answer = 'yes'
-        else:
-            answer = 'no'
-        game_data.append((question, answer))
-    engine.main(game_task, game_data)
+def run_even_game(max_number):
+    question = random.randint(1, max_number)
+    if question % 2 == 0:
+        answer = 'yes'
+    else:
+        answer = 'no'
+    return (question, answer)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+from brain_games import engine
 from brain_games.games import even
 
 
 def main():
-    even.run_even_game()
+    game_task = even.GAME_TASK
+    amount_of_rounds = even.AMOUNT_OF_ROUNDS
+    max_number = even.MAX_NUMBER
+    game = even.run_even_game
+    engine.main(game_task, amount_of_rounds, max_number, game)
 
 
 if __name__ == '__main__':
