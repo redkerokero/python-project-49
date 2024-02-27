d9e28c586546befcb76220daf4ab778200d6fd5b
Dmitriy <kerokero@yandex.ru>
2024-02-27 11:34:30 +0000

    extract condition into function

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,9 +4,13 @@ GAME_TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 MAX_NUMBER = 99
 
 
+def is_even(question):
+    return question % 2 == 0
+
+
 def main(max_number):
     question = random.randint(1, max_number)
-    if question % 2 == 0:
+    if is_even(question):
         answer = 'yes'
     else:
         answer = 'no'
